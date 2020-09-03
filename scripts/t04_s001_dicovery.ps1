2d9d8c35c7c880c8f1598383145785e1f6833fcd
joyitcwb <inf@joyit.com.br>
2020-09-03 12:06:38 -0300

    Stage 40

---
+++ scripts/t04_s001_dicovery.ps1
@@ -0,0 +1,53 @@
+# Desenvolvido por Diego Cavalcante - 22/12/2017
+# Monitora atividade de Jobs do Iperius Backup
+
+Param(
+  [string]$select,
+  [string]$2
+)
+
+# VariÃ¡veis
+
+$dirjobs = "C:\ProgramData\IperiusBackup\Jobs"
+$dirlogs = "C:\ProgramData\IperiusBackup\Logs"
+
+if ( $select -eq 'JSONJOBS' )
+{
+$comando = Get-ChildItem "$dirjobs" | Select Basename
+
+$comparador = 1
+write-host "{"
+write-host " `"data`":[`n"
+foreach ($objeto in $comando)
+{
+  $Name = [string]$objeto.BaseName
+    if ($comparador -lt $comando.Count)
+    {
+    $line= "{ `"{#JOBCONF}`" : `"" + $Name + "`" },"
+    write-host $line
+    }
+    elseif ($comparador -ge $comando.Count)
+    {
+    $line= "{ `"{#JOBCONF}`" : `"" + $Name + "`" }"
+    write-host $line
+    }
+    $comparador++;
+}
+write-host
+write-host " ]"
+write-host "}"
+}
+
+# Coleta o nome do Job
+if ( $select -eq 'JOBNOME' )
+{
+type $dirjobs\$2.ibj | FindStr "NAME=" | ForEach-Object {$_ -Replace "NAME=", ""}
+}
+
+# Coleta o ultimo status do Job
+if ( $select -eq 'JOBSTATUS' )
+{
+type $dirjobs\$2.ibj | FindStr "LastResult=" | ForEach-Object {$_ -Replace "LastResult=", ""}
+}
+
+

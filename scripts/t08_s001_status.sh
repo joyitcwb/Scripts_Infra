a57d96faf6a55fd8917138870b4ebc73ee9051fa
martius-joyit <60360515+martius-joyit@users.noreply.github.com>
2020-11-18 17:31:09 -0300

    Estado do nó do MySQLCluster
    Esto do nó do MySQL Cluster - wsrep_local_state_comment

--- scripts/t08_s001_status.sh
+++ scripts/t08_s001_status.sh
@@ -1,42 +1,4 @@
 #!/bin/bash
 
-
-export MYSQL_PWD=Lvam2@*es*; mysql -uroot -e "show status like 'wsrep%';" > /tmp/wsrepout.txt
-
-if [ "$1" = "wsrep_lsc" ]; then
-     if [ -e /tmp/wsrepout.txt ]; then
-       if cat /tmp/wsrepout.txt | grep -q "wsrep_local_state_comment    Synced"; then
-       echo "OK"
-       else
-       echo "FAIL"
-       fi
-     else
-     echo "NF"
-     fi
-
-
-elif [ "$1" = "wsrep_cw" ]; then
-     if [ -e /tmp/wsrepout.txt ]; then
-       if cat /tmp/wsrepout.txt | grep -q "wsrep_cluster_size   3"; then
-       echo "OK"
-       else
-       echo "FAIL"
-       fi
-     else
-     echo "NF"
-     fi
-
-elif [ "$1" = "wsrep_es" ]; then
-     if [ -e /tmp/wsrepout.txt ]; then
-       if cat /tmp/wsrepout.txt | grep -q "wsrep_evs_state      OPERATIONAL"; then
-       echo "OK"
-       else
-       echo "FAIL"
-       fi
-     else
-     echo "NF"
-     fi
-
-else
-     echo "NF"
-fi
+# Mostra o estado do nó do MySQL Cluster
+export MYSQL_PWD=Lvam2@*es*; mysql -uroot -e "show status like 'wsrep%';" | grep "wsrep_local_state_comment" | sed 's/\s\+/ /g' | cut -d' ' -f2

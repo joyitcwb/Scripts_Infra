4d052de3b6e0b93e5abf6752ca922fe745291895
martius-joyit <60360515+martius-joyit@users.noreply.github.com>
2021-01-19 13:20:55 -0300

    Update t08_s001_status.sh
    #!/usr/bin/env bash

--- scripts/t08_s001_status.sh
+++ scripts/t08_s001_status.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Mostra o estado do nó do MySQL Cluster
 export MYSQL_PWD=Lvam2@*es*; mysql -uroot -e "show status like 'wsrep%';" | grep "wsrep_local_state_comment" | sed 's/\s\+/ /g' | cut -d' ' -f2
